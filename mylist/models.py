71d254014c0e500456f13288a64f5cbd603ab769
Mathilda Heise <mathilda.heise@student.hpi.de>
2024-11-14 12:35:07 +0100

    added database and implemented logic for adding items

--- mylist/models.py
+++ mylist/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from datetime import date
+from django.utils import timezone
+
 
 # Create your models here.
+class ListItem(models.Model):
+    created_at = models.DateField(default=date.today)
+    item_text = models.CharField(max_length=200)
+    done = models.BooleanField(default=False)
--- mylist/views.py
+++ mylist/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import ListItem
 
 # Create your views here.
 def list(request):
-    return render(request, 'mylist/index.html')
+    if request.method == 'POST':
+        print('Received data:', request.POST['itemText'])
+        ListItem.objects.create(item_text = request.POST['itemText'])
+    all_items = ListItem.objects.all()
+    return render(request, 'mylist/index.html', {'all_items': all_items})
     # return HttpResponse("You are looking at you're to do list that is beeing build right now!")
